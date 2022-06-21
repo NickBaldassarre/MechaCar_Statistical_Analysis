7af6ef4cd31a671d5d113d74480f32b751e48c79
Nick Baldassarre <97616788+NickBaldassarre@users.noreply.github.com>
2022-06-21 06:50:27 -0400

    Updated R Script and uploaded images for Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,13 @@
 library(dplyr)
 
-MechaCar_mpg <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) #read in dataset
+mechampg <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) #read in dataset
 
-lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg) #perform linear regression
+lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechampg) #perform linear regression
 
-summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)) #summarize linear regression
+summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechampg)) #summarize linear regression
+
+suspension <- read.csv('Suspension_Coil.csv',stringsAsFactors = F) #read in dataset
+
+total_summary <- suspension %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #summarize PSI statistics
+
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #group summaries by lot
