94651d65f3aa9b506908ef7c5d840e64f7b34773
Nick Baldassarre <97616788+NickBaldassarre@users.noreply.github.com>
2022-06-21 06:18:47 -0400

    Delete MechaCarChallenge.RScript.R

--- MechaCarChallenge.RScript.R
+++
@@ -1,7 +0,0 @@
-library(dplyr)
-
-MechaCar_mpg <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) #read in dataset
-
-lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg) #perform linear regression
-
-summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)) #summarize linear regression
